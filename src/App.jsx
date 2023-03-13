2022f62ec29a91f78decabe9d0cd79f5fd7198bc
tae <rudxo5377@gmail.com>
2023-03-13 20:54:11 +0900

    9. react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 //dependencies
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+//router
+//switch component 는 match 하는 path 중에서 첫번째것만 return 하고 멈추게 함 >> routers wrap 하는 것으로 update 됨
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+//a tag 대신 link component 를 사용하고 href 대신 to 를 사용함
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id="logo" href="#">
+        <Link id="logo" to="/">
           Capture
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="#">1. About Us</a>
+          <Link to="/">1. About Us</Link>
         </li>
         <li>
-          <a href="#">2. Our Work</a>
+          <Link to="/work">2. Our Work</Link>
         </li>
         <li>
-          <a href="#">3. Contact Us</a>
+          <Link to="/contact">3. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
