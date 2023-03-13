a66775525c9f3f42f7e5694dd93ace115a8c2fc7
tae <rudxo5377@gmail.com>
2023-03-13 20:22:03 +0900

    8. creating Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+//dependencies
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
