ff269d9cd2f32c436305ce9724c17c9908fcad58
teddidev <rudxo5377@gmail.com>
2023-03-14 23:54:51 +0900

    23. nav line animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import styled from "styled-components";
 //a tag 대신 link component 를 사용하고 href 대신 to 를 사용함
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const { pathname } = useLocation();
   return (
     <StyledNav>
       <h1>
@@ -14,12 +17,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">1. About Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0" }}
+            animate={{ width: pathname === "/" ? "50%" : "0" }}
+          />
         </li>
         <li>
           <Link to="/work">2. Our Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0" }}
+            animate={{ width: pathname === "/work" ? "50%" : "0" }}
+          />
         </li>
         <li>
           <Link to="/contact">3. Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0" }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -53,7 +71,7 @@ const StyledNav = styled.div`
   }
   li {
     padding-left: 10rem;
-    postion: relative;
+    position: relative;
   }
   @media (max-width: 1300px) {
     flex-direction: column;
@@ -71,6 +89,17 @@ const StyledNav = styled.div`
         padding: 0;
       }
     }
+  }
+`;
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+    left: 0%;
   }
 `;
 
