1e13edc037f86266ed8fb35480c6fbfd3ffdac5f
tae <rudxo5377@gmail.com>
2023-03-14 16:27:33 +0900

    13. about animation

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
 
