7871da735b2ff20bffd3c72e7e78337f1143837b
teddidev <rudxo5377@gmail.com>
2023-03-14 18:32:58 +0900

    16. reusable component(toggle)

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
+  const [faqToggle, setFaqToggle] = useState(false);
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
-            soluta.
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus, soluta.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
       <div className="question">
         <h4>Daily schedule</h4>
         <div className="answer">
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,18 @@
+//reusable component
+
+import React, { useState } from "react";
+
+const Toggle = ({ children }) => {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <div
+      onClick={() => {
+        setToggle(!toggle);
+      }}
+    >
+      {toggle ? children : ""}
+    </div>
+  );
+};
+
+export default Toggle;
